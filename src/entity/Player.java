24d9d7dd92cd897dd34a7a7596f4477ef9058791
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-04-17 16:27:57 +0200

    Add player walk animations

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -25,11 +25,22 @@ public class Player extends Entity{
     x = 100;
     y = 100;
     speed = 4;
+    direction = "down";
   }
 
+  /**
+   * Load player images.
+   */
   public void getPlayerImage() {
     try {
-      def = ImageIO.read(getClass().getResourceAsStream("/player/playerdef.png"));
+      up1 = ImageIO.read(getClass().getResourceAsStream("/player/player_up1.png"));
+      up2 = ImageIO.read(getClass().getResourceAsStream("/player/player_up2.png"));
+      down1 = ImageIO.read(getClass().getResourceAsStream("/player/player_down1.png"));
+      down2 = ImageIO.read(getClass().getResourceAsStream("/player/player_down2.png"));
+      left1 = ImageIO.read(getClass().getResourceAsStream("/player/player_left1.png"));
+      left2 = ImageIO.read(getClass().getResourceAsStream("/player/player_left2.png"));
+      right1 = ImageIO.read(getClass().getResourceAsStream("/player/player_right1.png"));
+      right2 = ImageIO.read(getClass().getResourceAsStream("/player/player_right2.png"));
     } catch(IOException e) {
       e.printStackTrace();
     }
@@ -37,19 +48,68 @@ public class Player extends Entity{
 
   public void update() {
     if(keyHand.upPressed == true) {
+      direction = "up";
       y = y - speed;// move player up
     } else if (keyHand.downPressed == true) {
+      direction = "down";
       y = y + speed; // move player down
     } else if (keyHand.leftPressed == true) {
+      direction = "left";
       x = x - speed;// move player left
     } else if (keyHand.rightPressed == true) {
+      direction = "right";
       x = x + speed; // move player right
+    }
+
+    spriteCount++;
+    if (spriteCount > 15) {
+      if (spriteNum == 1) {
+        spriteNum = 2;
+      } else if (spriteNum == 2) {
+        spriteNum = 1;
+      }
+      spriteCount = 0;
     }
   }
 
   public void draw(Graphics2D g2d) {
+    BufferedImage image = null;
 
-    BufferedImage image = def;
+    switch(direction) {
+      case "up":
+        if (spriteNum == 1) {
+          image = up1;
+        } 
+        if (spriteNum == 2) {
+          image = up2;
+        }
+        break;
+      case "down":
+      if (spriteNum == 1) {
+        image = down1;
+      } 
+      if (spriteNum == 2) {
+        image = down2;
+      }
+        break;
+      case "left":
+      if (spriteNum == 1) {
+        image = left1;
+      } 
+      if (spriteNum == 2) {
+        image = left2;
+      }
+        break;
+      case "right":
+      if (spriteNum == 1) {
+        image = right1;
+      } 
+      if (spriteNum == 2) {
+        image = right2;
+      }
+        break;
+    }
+
     g2d.drawImage(image, x, y, gamePan.tileSize, gamePan.tileSize, null);
 
   }
