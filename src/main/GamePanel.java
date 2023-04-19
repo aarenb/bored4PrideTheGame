b4674fc936bec23058f20ad82cda5ee8df72b23c
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-04-19 13:16:10 +0200

    Add tile images

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -25,11 +25,6 @@ public class GamePanel extends JPanel implements Runnable{
   Thread gameThread;// keeps the game running
   Player player = new Player(this, keyHand);
 
-  // Player default position:
-  int playerX = 100;
-  int playerY = 100;
-  int playerSpeed = 4;
-
   public GamePanel() {
     this.setPreferredSize(new Dimension(screenWidth, screenHeight));
     this.setBackground(Color.black);
