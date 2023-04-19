1a0d15a128bb1c2fed974b87a9acf3f4a626bdab
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-04-19 14:03:44 +0200

    Add fileNames and collisionStat arraylists

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -1,15 +1,40 @@
 package tile;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 import main.GamePanel;
 
 public class TileManager {
   GamePanel gamePan;
   Tile[] tile;
+  ArrayList<String> fileNames = new ArrayList<>();
+  ArrayList<String> collisionStat = new ArrayList<>();
 
   public TileManager(GamePanel gamePan) {
     this.gamePan = gamePan;
-    tile = new Tile[250];
 
+    // Read tile data file
+    InputStream input = getClass().getResourceAsStream("/maps/tilesdata.txt");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+    // Get tile names + collission info from file data
+    String line;
+
+    try {
+      while((line = reader.readLine()) != null) {
+        fileNames.add(line);
+        collisionStat.add(reader.readLine());
+      }
+      reader.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    tile = new Tile[fileNames.size()];
     getTileImage();
   }
 
