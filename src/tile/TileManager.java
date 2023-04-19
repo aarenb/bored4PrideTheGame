1138ba6ffd1db61ce25bd45cedd494b385749332
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-04-19 13:30:58 +0200

    Add TileManager class

---
+++ src/tile/TileManager.java
@@ -0,0 +1,13 @@
+package tile;
+
+import main.GamePanel;
+
+public class TileManager {
+  GamePanel gamePan;
+  Tile[] tile;
+
+  public TileManager(GamePanel gamePan) {
+    this.gamePan = gamePan;
+    tile = new Tile[250];
+  }
+}
