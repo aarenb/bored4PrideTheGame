63efff17fb43ef150af9fd7f6d23c8c993f1e26c
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-04-20 10:20:03 +0200

    Add UI class

---
+++ src/main/UI.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.awt.Graphics2D;
+
+public class UI {
+  GamePanel gamePan;
+
+  public UI(GamePanel gamePan) {
+    this.gamePan = gamePan;
+  }
+
+  public void draw(Graphics2D g2d) {
+    
+  }
+}
