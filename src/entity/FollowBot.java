23dceb1fa1c6e56495e9328adcbf38234e1d6ea8
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-04-24 11:37:35 +0200

    Set followbot variables in constructor

--- src/entity/FollowBot.java
+++ src/entity/FollowBot.java
@@ -1,11 +1,21 @@
 package entity;
 
+import java.awt.Rectangle;
+
 import main.GamePanel;
 
 public class FollowBot extends Entity {
 
   public FollowBot(GamePanel gamePan) {
     super(gamePan);
+
+    speed = 1;
+    maxLife = 4;
+    life = maxLife;
+
+    solidArea = new Rectangle(0, 0, 48, 48); // TODO: change???
+    solidAreaDefaultX = solidArea.x;
+    solidAreaDefaultY = solidArea.y;
   }
   
 }
