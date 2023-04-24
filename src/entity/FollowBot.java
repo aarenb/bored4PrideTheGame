53541fb2e362011764ad9cd0c1aac7ad5ec2360d
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-04-24 11:41:51 +0200

    Add getImages to FollowBot

--- src/entity/FollowBot.java
+++ src/entity/FollowBot.java
@@ -17,5 +17,18 @@ public class FollowBot extends Entity {
     solidAreaDefaultX = solidArea.x;
     solidAreaDefaultY = solidArea.y;
   }
-  
+
+  /**
+   * Load follow bot images
+   */
+  public void getImage() {
+    up1 = setup("/resources/followbot/followbot_up1");
+    up2 = setup("/resources/followbot/followbot_up2");
+    down1 = setup("/resources/followbot/followbot_down1");
+    down2 = setup("/resources/followbot/followbot_down2");
+    left1 = setup("/resources/followbot/followbot_left1");
+    left2 = setup("/resources/followbot/followbot_left2");
+    right1 = setup("/resources/followbot/followbot_right1");
+    right2 = setup("/resources/followbot/followbot_right2");
+  }
 }
