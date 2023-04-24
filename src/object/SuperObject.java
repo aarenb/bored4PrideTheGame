73e1a0c476b30858315980936b82afb83cd6042b
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-04-24 08:21:12 +0200

    Add OBJ_Heart

--- src/object/SuperObject.java
+++ src/object/SuperObject.java
@@ -2,6 +2,10 @@ package object;
 
 import java.awt.image.BufferedImage;
 
+import main.UtilityTool;
+
 public class SuperObject {
   public BufferedImage image, image2, image3;
+  public String name;
+  UtilityTool uTool = new UtilityTool();
 }
