40fac9935cd5c5514d507146f76ca68a194eb026
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-04-24 08:24:47 +0200

    Add constructor to OBJ

--- src/object/OBJ_Heart.java
+++ src/object/OBJ_Heart.java
@@ -1,5 +1,27 @@
 package object;
 
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import main.GamePanel;
+
 public class OBJ_Heart extends SuperObject{
+  GamePanel gamePan;
   
+  public OBJ_Heart(GamePanel gamePan) {
+    this.gamePan = gamePan;
+    name = "heart";
+
+    try{
+      image = ImageIO.read(getClass().getResourceAsStream("/resources/objects/heart_full.png"));
+      image2 = ImageIO.read(getClass().getResourceAsStream("/resources/objects/heart_half.png"));
+      image3 = ImageIO.read(getClass().getResourceAsStream("/resources/objects/heart_empty.png"));
+      uTool.scaleImage(image, gamePan.tileSize, gamePan.tileSize);
+      uTool.scaleImage(image2, gamePan.tileSize, gamePan.tileSize);
+      uTool.scaleImage(image3, gamePan.tileSize, gamePan.tileSize);
+    } catch(IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
