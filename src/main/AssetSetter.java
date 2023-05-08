e2aa7efeb45c2e573fc61365532fa4ca37c6b9cc
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-05-08 15:46:51 +0200

    Display a bit on the ground

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -2,6 +2,7 @@ package main;
 
 import entity.FollowBot;
 import entity.NPC_Wongy;
+import object.OBJ_Bit;
 import object.OBJ_ModSword;
 
 public class AssetSetter {
@@ -31,5 +32,8 @@ public class AssetSetter {
     gamePan.obj[0] = new OBJ_ModSword(gamePan);
     gamePan.obj[0].worldX = gamePan.tileSize * 19;
     gamePan.obj[0].worldY = gamePan.tileSize * 32;
+    gamePan.obj[1] = new OBJ_Bit(gamePan);
+    gamePan.obj[1].worldX = gamePan.tileSize * 24;
+    gamePan.obj[1].worldY = gamePan.tileSize * 40;
   }
 }
