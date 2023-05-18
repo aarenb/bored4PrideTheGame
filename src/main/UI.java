06cb0b4580d7299e867feaa212aefc700f4f7ff1
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-05-18 15:18:00 +0200

    Add version to title screen

--- src/main/UI.java
+++ src/main/UI.java
@@ -256,6 +256,12 @@ public class UI {
       g2d.drawString(">", x - gamePan.tileSize, y);
     }
 
+    g2d.setFont(VCR_OSD_Mono_28);
+    g2d.setColor(Color.white);
+    x = gamePan.screenWidth - gamePan.tileSize * 3 + - 20;
+    y = gamePan.screenHeight - 17;
+    text = "Alpha 1.0";
+    g2d.drawString(text, x, y);
   }
 
   public void drawControlsScreen() {
