0d7e07380a0e3bcbc87a5f4ee4672c03f1bd7dc6
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-05-19 10:51:17 +0200

    Comment UtilityTool

--- src/main/UtilityTool.java
+++ src/main/UtilityTool.java
@@ -3,8 +3,19 @@ package main;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
+/**
+ * Represents a utility tool.
+ */
 public class UtilityTool {
 
+  /**
+   * Scales an image.
+   *
+   * @param og Original image
+   * @param width Width to scale image to
+   * @param height Height to scale image to
+   * @return The scaled image
+   */
   public BufferedImage scaleImage(BufferedImage og, int width, int height) {
     BufferedImage scaledImg = new BufferedImage(width, height, og.getType());
     Graphics2D g2d = scaledImg.createGraphics();
