14a21c903419958705a2e4c4a56fd98ed0e16ffc
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-05-19 13:46:48 +0200

    Update OBJ_Heart

--- src/object/OBJ_Heart.java
+++ src/object/OBJ_Heart.java
@@ -11,8 +11,19 @@ public class OBJ_Heart extends Entity{
   public OBJ_Heart(GamePanel gamePan) {
     super(gamePan);
 
+    name = "heart";
+
     image = setup("/resources/objects/heart_full", gamePan.tileSize, gamePan.tileSize);
     image2 = setup("/resources/objects/heart_half", gamePan.tileSize, gamePan.tileSize);
     image3 = setup("/resources/objects/heart_empty", gamePan.tileSize, gamePan.tileSize);
+    down1 = image; // image when it's dropped on map
+  }
+
+  public void use(Entity entity) {
+    entity.life += 2;
+
+    if (entity.life > entity.maxLife) {
+      entity.life = entity.maxLife;
+    }
   }
 }
