eaa9390e08c3b6b15fe0c04b352dddc52a6686a3
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-05-26 12:25:07 +0200

    Add save method to SaveLoad

--- src/data/SaveLoad.java
+++ src/data/SaveLoad.java
@@ -1,5 +1,10 @@
 package data;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import main.GamePanel;
 
 public class SaveLoad {
@@ -7,5 +12,25 @@ public class SaveLoad {
 
   public SaveLoad(GamePanel gamePan) {
     this.gamePan = gamePan;
+  }
+
+  public void save() {
+    try {
+      ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(new File("save.dat")));
+      DataStorage storage = new DataStorage();
+
+      // Save player stats
+      storage.life = gamePan.player.life;
+      storage.worldX = gamePan.player.worldX;
+      storage.worldY = gamePan.player.worldY;
+      storage.bits = gamePan.player.bits;
+      storage.hasSword = gamePan.player.hasSword;
+
+      // Write data to file
+      output.writeObject(storage);
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
