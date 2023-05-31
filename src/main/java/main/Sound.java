7c8ae1d774af7bf8cf8adbfb4217a8dce4e6a2be
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-05-31 19:01:49 +0200

    Fix checkstyle for Sound

--- src/main/java/main/Sound.java
+++ src/main/java/main/Sound.java
@@ -1,7 +1,6 @@
 package main;
 
 import java.net.URL;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -12,46 +11,49 @@ import javax.sound.sampled.FloatControl;
  */
 public class Sound {
   Clip clip;
-  URL soundURL[] = new URL[30];
+  URL[] soundUrl = new URL[30];
   FloatControl fc;
   int volumeScale = 3;
   float volume;
   
+  /**
+   * Creates a sound object.
+   */
   public Sound() {
     // Music
-    soundURL[0] = getClass().getResource("/sound/Voyage.wav");
-    soundURL[1] = getClass().getResource("/sound/PixelParty.wav");
+    soundUrl[0] = getClass().getResource("/sound/Voyage.wav");
+    soundUrl[1] = getClass().getResource("/sound/PixelParty.wav");
 
     // Sound effects
-    soundURL[2] = getClass().getResource("/sound/cursor.wav");
-    soundURL[3] = getClass().getResource("/sound/bit.wav");
-    soundURL[4] = getClass().getResource("/sound/sword.wav");
-    soundURL[5] = getClass().getResource("/sound/oof.wav");
-    soundURL[6] = getClass().getResource("/sound/gameOver.wav");
-    soundURL[7] = getClass().getResource("/sound/sparkle.wav");
-    soundURL[8] = getClass().getResource("/sound/botDamage.wav");
-    soundURL[9] = getClass().getResource("/sound/botDeath.wav");
-    soundURL[10] = getClass().getResource("/sound/talk.wav");
+    soundUrl[2] = getClass().getResource("/sound/cursor.wav");
+    soundUrl[3] = getClass().getResource("/sound/bit.wav");
+    soundUrl[4] = getClass().getResource("/sound/sword.wav");
+    soundUrl[5] = getClass().getResource("/sound/oof.wav");
+    soundUrl[6] = getClass().getResource("/sound/gameOver.wav");
+    soundUrl[7] = getClass().getResource("/sound/sparkle.wav");
+    soundUrl[8] = getClass().getResource("/sound/botDamage.wav");
+    soundUrl[9] = getClass().getResource("/sound/botDeath.wav");
+    soundUrl[10] = getClass().getResource("/sound/talk.wav");
 
     // More music ;)
-    soundURL[11] = getClass().getResource("/sound/teehee.wav");
+    soundUrl[11] = getClass().getResource("/sound/teehee.wav");
   }
 
   /**
    * Opens an audio file.
    *
-   * @param int Index of audio file.
+   * @param i Index of audio file.
    */
-    public void setFile(int i) {
-      try {
-        AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
-        clip = AudioSystem.getClip();
-        clip.open(ais);
-        fc = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
-        checkVolume();
-      } catch (Exception e) {
-
-      }
+  public void setFile(int i) {
+    try {
+      AudioInputStream ais = AudioSystem.getAudioInputStream(soundUrl[i]);
+      clip = AudioSystem.getClip();
+      clip.open(ais);
+      fc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+      checkVolume();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   /**
@@ -97,6 +99,8 @@ public class Sound {
         break;
       case 5:
         volume = 6f;
+        break;
+      default:
         break;
     }
     fc.setValue(volume);
