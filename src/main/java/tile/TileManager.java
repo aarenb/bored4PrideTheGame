81bcf2e2c74a26f578003a8efe094781e5d899cb
Aaren Bertilsson <ab226gw@student.lnu.se>
2023-05-31 19:18:50 +0200

    Fix checkstyle for TileManager

--- src/main/java/tile/TileManager.java
+++ src/main/java/tile/TileManager.java
@@ -16,10 +16,15 @@ import main.UtilityTool;
 public class TileManager {
   GamePanel gamePan;
   public Tile[] tile;
-  public int mapTileNum[][];
+  public int[][] mapTileNum;
   ArrayList<String> fileNames = new ArrayList<>();
   ArrayList<String> collisionStat = new ArrayList<>();
 
+  /**
+   * Creates a tile manager object.
+   *
+   * @param gamePan The game's GamePanel.
+   */
   public TileManager(GamePanel gamePan) {
     this.gamePan = gamePan;
 
@@ -31,7 +36,7 @@ public class TileManager {
     String line;
 
     try {
-      while((line = reader.readLine()) != null) {
+      while ((line = reader.readLine()) != null) {
         fileNames.add(line);
         collisionStat.add(reader.readLine());
       }
@@ -70,19 +75,20 @@ public class TileManager {
 
   /**
    * Sets up tile.
+   *
    * @param index Index of new tile.
    * @param imageName Name of tile image.
    * @param collision If collision is true or false.
    */
   public void setup(int index, String imageName, boolean collision) {
-    UtilityTool uTool = new UtilityTool();
+    UtilityTool utool = new UtilityTool();
 
     try {
       tile[index] = new Tile();
       tile[index].image = ImageIO.read(getClass().getResourceAsStream("/tiles/" + imageName));
-      tile[index].image = uTool.scaleImage(tile[index].image, gamePan.tileSize, gamePan.tileSize);
+      tile[index].image = utool.scaleImage(tile[index].image, gamePan.tileSize, gamePan.tileSize);
       tile[index].collision = collision;
-    } catch (IOException e){
+    } catch (IOException e) {
       e.printStackTrace();
     }
   }
@@ -102,7 +108,7 @@ public class TileManager {
         String line = reader.readLine();
 
         while (col < gamePan.maxWorldColumn) {
-          String numbers[] = line.split(" ");
+          String[] numbers = line.split(" ");
 
           int num = Integer.parseInt(numbers[col]);
           mapTileNum[col][row] = num;
@@ -116,7 +122,7 @@ public class TileManager {
       }
       reader.close();
     } catch (Exception e) {
-
+      e.printStackTrace();
     }
   }
 
@@ -137,11 +143,11 @@ public class TileManager {
       int screenX = worldX - gamePan.player.worldX + gamePan.player.screenX;
       int screenY = worldY - gamePan.player.worldY + gamePan.player.screenY;
 
-      if (worldX + gamePan.tileSize > gamePan.player.worldX - gamePan.player.screenX &&
-          worldX - gamePan.tileSize < gamePan.player.worldX + gamePan.player.screenX && 
-          worldY + gamePan.tileSize > gamePan.player.worldY - gamePan.player.screenY && 
-          worldY - gamePan.tileSize < gamePan.player.worldY + gamePan.player.screenY) {
-            g2d.drawImage(tile[tileNum].image, screenX, screenY, null);
+      if (worldX + gamePan.tileSize > gamePan.player.worldX - gamePan.player.screenX
+          && worldX - gamePan.tileSize < gamePan.player.worldX + gamePan.player.screenX
+          && worldY + gamePan.tileSize > gamePan.player.worldY - gamePan.player.screenY
+          && worldY - gamePan.tileSize < gamePan.player.worldY + gamePan.player.screenY) {
+        g2d.drawImage(tile[tileNum].image, screenX, screenY, null);
       }
       worldCol++;  
 
